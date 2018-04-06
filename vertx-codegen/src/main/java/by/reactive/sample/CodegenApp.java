cae8ef5b7d6688e56709e0d20e2c99dda6a8e467
dshybeka <dzianis_shybeka@epam.com>
2018-04-06 20:05:26 +0300

    Added vert.x codegen

---
+++ vertx-codegen/src/main/java/by/reactive/sample/CodegenApp.java
@@ -0,0 +1,28 @@
+package by.reactive.sample;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.reactivex.core.Vertx;
+
+public class CodegenApp {
+
+    public static final String CODEGEN_PROCESSING_ADDRESS = "codegen-worker";
+
+    private static final Logger log = LoggerFactory.getLogger("App.java");
+
+    public static void main(String[] args) {
+
+        log.info("Starting application");
+
+        Vertx vertx = Vertx.vertx();
+
+        vertx.rxDeployVerticle(WorkerVerticle.class.getName(), new DeploymentOptions().setWorker(true))
+             .subscribe(ok -> {
+
+                 vertx.deployVerticle(HttpVerticle.class.getName());
+
+                 log.info("Application started");
+             }, error -> log.error("Cannot start application ", error));
+    }
+}
